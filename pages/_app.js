28aed837b2f07af83226fb10f4c377d0ca0a6797
diegodreossi <diego.dreossi@fatec.sp.gov.br>
2022-01-26 11:39:30 -0300

    terceiro commit

--- pages/_app.js
+++ pages/_app.js
@@ -32,7 +32,7 @@ function GlobalStyle() {
 }
 
 export default function MyApp({Component, pageProps}){
-    console.log('Roda em todas as página');
+    //console.log('Roda em todas as página');
     return (
         <>
             <GlobalStyle/>
